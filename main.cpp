18f93e6ff4e2178ecfddd47e90a5c00b768374a7
mathias <mathiasnextkbh@gmail.com>
2022-12-08 11:02:03 +0100

    bruh

--- main.cpp
+++ main.cpp
@@ -6,20 +6,23 @@ class Main {
 		if ( n == 1 ) { return false; }
 		if ( 1 < n & n < 4 ) { return true; }
 		if ( n % 2 == 0 ) { return false; }
-		int m = std::ceil( sqrt(n) );
-		for ( int i = 3; i < m; i += 2 ) {
+		for ( int i = 3; i*i >= n; i += 2 ) {
 			if ( n % i == 0 ) { return false; }
 		}
 		return true;
 	};
 };
 
-int main() {
+int main () {
 	Main Main;
-	for (int i = 1; i < 100; i++) {
+	int x;
+	std::cout << "Range: "; 
+	std::cin >> x;
+	for (int i = 1; i <= x; i++) {
 		if ( Main.isPrime(i) ) {
-			std::cout << i << " is a prime\n";
+			std::cout << i << " ";
 		}
 	}
+	std::cout << "\n";
 	return 0;
 };
