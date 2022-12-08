ee46a476776127d0bfdbecdb8e3f3851f976c991
mathias <mathiasnextkbh@gmail.com>
2022-12-08 13:07:53 +0100

    update idk

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 class Main {
-	public: bool isPrime ( int n ) {
-		if ( n == 1 ) { return false; }
-		if ( 1 < n & n < 4 ) { return true; }
-		if ( n % 2 == 0 ) { return false; }
-		for ( int i = 3; i*i >= n; i += 2 ) {
-			if ( n % i == 0 ) { return false; }
-		}
-		return true;
-	};
+	public: 
+		bool isPrime ( int n ) {
+			if ( n == 1 ) { return false; }
+			if ( 1 < n & n < 4 ) { return true; }
+			if ( n % 2 == 0 ) { return false; }
+			for ( int i = 3; i*i >= n; i += 2 ) {
+				if ( n % i == 0 ) { return false; }
+			}
+			return true;
+		};
 };
 
 int main () {
 	Main Main;
 	int x;
-	std::cout << "Range: "; 
-	std::cin >> x;
+	cout << "Range: "; cin >> x;
 	for (int i = 1; i <= x; i++) {
 		if ( Main.isPrime(i) ) {
-			std::cout << i << " ";
+			cout << i << " ";
 		}
 	}
-	std::cout << "\n";
+	cout << "\n";
 	return 0;
 };
